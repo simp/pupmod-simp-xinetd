181793a9d098d3cb6ea0a111a4242400d80504b4
Liz Nemsick <lnemsick.simp@gmail.com>
2016-11-22 15:21:22 -0500

    Use test facts to fix travis tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,32 +1,34 @@
 require 'spec_helper'
 
 describe 'xinetd' do
+ context 'supported operating systems' do
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
+        let(:facts){os_facts}
 
-  base_facts = {
-    :interfaces => 'eth0'
-  }
-  let(:facts){base_facts}
+        it { is_expected.to create_class('xinetd') }
+        it { is_expected.to contain_package('xinetd').that_comes_before('Service[xinetd]') }
+        it { is_expected.to contain_package('xinetd').that_comes_before('File[/etc/xinetd.conf]') }
+        it { is_expected.to contain_package('xinetd').that_comes_before('File[/etc/xinetd.d]') }
 
-  it { is_expected.to create_class('xinetd') }
-  it { is_expected.to contain_package('xinetd').that_comes_before('Service[xinetd]') }
-  it { is_expected.to contain_package('xinetd').that_comes_before('File[/etc/xinetd.conf]') }
-  it { is_expected.to contain_package('xinetd').that_comes_before('File[/etc/xinetd.d]') }
+        it do
+          is_expected.to contain_file('/etc/xinetd.conf').with({
+            'owner' => 'root',
+            'group' => 'root',
+            'mode' => '0600',
+          })
+        end
 
-  it do
-    is_expected.to contain_file('/etc/xinetd.conf').with({
-      'owner' => 'root',
-      'group' => 'root',
-      'mode' => '0600',
-    })
-  end
-
-  it do
-    is_expected.to contain_file('/etc/xinetd.d').with({
-      'ensure' => 'directory',
-      'owner' => 'root',
-      'group' => 'root',
-      'mode' => '0640',
-      'recurse' => 'true'
-    })
+        it do
+          is_expected.to contain_file('/etc/xinetd.d').with({
+            'ensure' => 'directory',
+            'owner' => 'root',
+            'group' => 'root',
+            'mode' => '0640',
+            'recurse' => 'true'
+          })
+        end
+      end
+    end
   end
 end
